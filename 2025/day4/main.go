7df1d06988b4282b5d0ff9a431217dcf8db2c8b4
Sebastian Holmgaard <seb@4xs.dk>
2025-12-04 09:00:07 +0100

    2025 d4

--- 2025/day4/main.go
+++ 2025/day4/main.go
@@ -1,1 +1,109 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	f1, _ := os.Open("2025/day4/input.txt")
+	defer f1.Close()
+	now := time.Now()
+	result1 := part1(f1)
+	elapsed := time.Since(now)
+	fmt.Println("Part 1:", result1, "Time:", elapsed)
+
+	f2, _ := os.Open("2025/day4/input.txt")
+	defer f2.Close()
+	now = time.Now()
+	result2 := part2(f2)
+	elapsed = time.Since(now)
+	fmt.Println("Part 2:", result2, "Time:", elapsed)
+}
+
+func getAdjacent(grid []string, x, y int) []byte {
+	adjacent := make([]byte, 0)
+	directions := [][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	for _, dir := range directions {
+		newX, newY := x+dir[0], y+dir[1]
+		if newX >= 0 && newX < len(grid[0]) && newY >= 0 && newY < len(grid) {
+			adjacent = append(adjacent, grid[newY][newX])
+		}
+	}
+
+	return adjacent
+}
+
+func part1(f *os.File) int {
+	result := 0
+
+	grid := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		grid = append(grid, scanner.Text())
+	}
+
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			if grid[y][x] != '@' {
+				continue
+			}
+			count := 0
+			for _, adj := range getAdjacent(grid, x, y) {
+				if adj == '@' {
+					count++
+				}
+			}
+			if count < 4 {
+				result++
+			}
+		}
+	}
+
+	return result
+}
+
+func part2(f *os.File) int {
+	result := 0
+	taken := make(map[[2]int]struct{})
+	grid := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		grid = append(grid, scanner.Text())
+	}
+
+doLoop:
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			if grid[y][x] != '@' {
+				continue
+			}
+			count := 0
+			for _, adj := range getAdjacent(grid, x, y) {
+				if adj == '@' {
+					count++
+				}
+			}
+			if count < 4 {
+				result++
+				taken[[2]int{x, y}] = struct{}{}
+			}
+		}
+	}
+	for pos := range taken {
+		grid[pos[1]] = grid[pos[1]][:pos[0]] + "." + grid[pos[1]][pos[0]+1:]
+	}
+	if len(taken) > 0 {
+		taken = make(map[[2]int]struct{})
+		goto doLoop
+	}
+
+	return result
+}
